d1ca4b5eeab7a79826d7ffb96ed67ed0bac4215d
elnebuloso <jan.thoennessen@googlemail.com>
2014-01-07 22:39:25 +0100

    powerpos

--- autoload.php
+++ autoload.php
@@ -3,7 +3,3 @@
 if(file_exists('vendor/autoload.php')) {
     $loader = include 'vendor/autoload.php';
 }
-
-$autoLoader = new \Zend\Loader\StandardAutoloader();
-$autoLoader->registerNamespace('Powerpos', __DIR__ . '/src/Powerpos');
-$autoLoader->register();
--- public/index.php
+++ public/index.php
@@ -1,24 +1,14 @@
 <?php
-// this makes our life easier when dealing with paths.
-// everything is relative to the application root now.
-chdir(dirname(__DIR__));
-
-// composer autoloading
-if(file_exists('vendor/autoload.php')) {
-    $loader = include 'vendor/autoload.php';
-}
-
 // error reporting
 error_reporting(E_ALL);
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 'on');
 
-// run hive
-$hive = new \Hive\Hive();
+// this makes our life easier when dealing with paths.
+// everything is relative to the application root now.
+chdir(dirname(__DIR__));
 
-$hive->getRouter()->createRoute('index')->when('^/$')->then(function ($hive) {
-    echo '<h1>powerpos</h1>';
-    echo \Powerpos\Version::getVersion();
-});
+// autoloading
+include 'autoload.php';
 
-$hive->run();
+die('powerpos');
